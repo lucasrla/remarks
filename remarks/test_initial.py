da5f062e3a0c912c75c8f075918c15e4c2c89619
Laura <mail@laura.fm>
2022-12-21 16:20:11 +0100

    Add: Initial test, running demo

---
+++ remarks/test_initial.py
@@ -0,0 +1,24 @@
+import remarks
+import os
+
+
+def test_can_process_on_computable_numbers():
+    initial_args = {
+        'file_name': None,
+        'ann_type': ['scribbles', 'highlights'],
+        'combined_pdf': True,
+        'combined_md': True,
+        'modified_pdf': False,
+        'md_hl_format': 'whole_block',
+        'md_page_offset': 0,
+        'md_header_format': 'atx',
+        'per_page_targets': [],
+        'assume_malformed_pdfs': False,
+        'avoid_ocr': False
+    }
+    remarks.run_remarks("demo/on-computable-numbers/xochitl", "tests/out", **initial_args)
+
+    assert os.path.isfile("tests/out/1936 On Computable Numbers, with an Application to the Entscheidungsproblem - A. "
+                          "M _remarks.pdf")
+    assert os.path.isfile("tests/out/1936 On Computable Numbers, with an Application to the Entscheidungsproblem - A. "
+                          "M _highlights.md")
